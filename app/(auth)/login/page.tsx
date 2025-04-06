7ca8fe5afdffc96b870b06eebad7ceb9f9553006
JonashGana <jonashtiglao@gmail.com>
2025-04-06 22:21:36 +0800

    added login page

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "@/components/auth/form/LoginForm";
+
+const Login = () => {
+  return (
+    <div className="flex flex-col w-full">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Login;
