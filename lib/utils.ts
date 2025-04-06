8a8ad5945cba04cbbb3c38b62e77b0cbe9144290
JonashGana <jonashtiglao@gmail.com>
2025-04-06 22:19:53 +0800

    updated

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const themes = [
+  { name: "Snowy Winter Cabin", image: "/themes/snowy-winter-cabin.jpg" },
+  { name: "Countryside Afternoon", image: "/themes/countryside-afternoon.jpg" },
+  { name: "Rainy Street Night", image: "/themes/rainy-street-night.jpg" },
+  { name: "Sakura Street", image: "/themes/sakura-street.jpg" },
+  { name: "Sunset Fall Season", image: "/themes/sunset-fall-season.jpg" },
+  { name: "Sunset Valley", image: "/themes/sunset-valley.jpg" },
+  { name: "Wisdom Forest", image: "/themes/wisdom-forest.jpg" },
+];
