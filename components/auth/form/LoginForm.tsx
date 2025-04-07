3450c099808a8bfefdee5437122c98501d351094
JonashGana <jonashtiglao@gmail.com>
2025-04-07 23:46:56 +0800

    updated

--- components/auth/form/LoginForm.tsx
+++ components/auth/form/LoginForm.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { Eye, EyeOff } from "lucide-react";
+import { login } from "@/actions/auth";
 import ContinueButton from "@/components/buttons/ContinueButton";
 import Link from "next/link";
 
@@ -15,6 +16,7 @@ const LoginForm = () => {
           <div>
             <input
               type="email"
+              name="email"
               placeholder="name@example.com"
               className="text-lg w-full border-b-2 border-zinc-800/50 focus:border-zinc-900 focus:outline-none transition-all duration-500 pr-2 py-2"
             />
@@ -22,6 +24,7 @@ const LoginForm = () => {
           <div className="flex items-center gap-2 border-b-2 border-zinc-800/50 focus-within:border-zinc-900 pr-2 py-2 focus:outline-none transition-all duration-300">
             <input
               type={showPassword ? "text" : "password"}
+              name="password"
               placeholder="Password"
               className="text-lg w-full focus:outline-none"
             />
