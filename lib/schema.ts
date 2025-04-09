50a5a86983f730d0947d01f4df863e38bc38d6d3
JonashGana <jonashtiglao@gmail.com>
2025-04-09 22:45:34 +0800

    updated

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,5 +1,14 @@
 import { z } from "zod";
 
+export const LoginSchema = z.object({
+  email: z.string().email({ message: "Please enter a valid email address" }),
+  password: z
+    .string()
+    .min(1, "Password is required")
+    .min(8, "Password must be at least 8 characters")
+    .max(100, "Password must not exceed 100 characters"),
+});
+
 export const RegisterSchema = z.object({
   firstName: z.string().min(1, "Please input your name"),
   email: z.string().email({ message: "Please enter a valid email address" }),
