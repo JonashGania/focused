01b340efbbd4a1ac307f9bd7174d6fedda09681f
JonashGana <jonashtiglao@gmail.com>
2025-04-11 23:53:10 +0800

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 import { signout } from "@/actions/auth";
+import BackgroundWrapper from "@/components/BackgroundWrapper";
 
 export default async function Home() {
   const supabase = await createClient();
@@ -11,16 +12,19 @@ export default async function Home() {
   }
 
   return (
-    <div className="w-full h-screen">
-      <div></div>
-      <div>
-        <h1>This is main page</h1>
-        <form action={signout}>
-          <button className="bg-neutral-900 text-white cursor-pointer px-5 py-2">
-            Sign Out
-          </button>
-        </form>
+    <>
+      <BackgroundWrapper />
+      <div className="w-full h-screen">
+        <div></div>
+        <div>
+          <h1 className="text-white">This is main page</h1>
+          <form action={signout}>
+            <button className="bg-white text-black cursor-pointer px-5 py-2">
+              Sign Out
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
