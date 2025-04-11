1bceffcdca23c18cc6c14b7989e00fbf02eaba7f
JonashGana <jonashtiglao@gmail.com>
2025-04-11 23:54:34 +0800

    remove layout background image

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,8 +2,7 @@ import React, { ReactNode } from "react";
 
 const layout = ({ children }: { children: ReactNode }) => {
   return (
-    <section className="w-full min-h-svh flex justify-center items-center bg-[url(/images/auth-bg.jpg)] bg-center bg-no-repeat bg-cover relative">
-      <div className="absolute inset-0 bg-black/15 z-0"></div>
+    <section className="w-full min-h-svh flex justify-center items-center bg-gray-100">
       {children}
     </section>
   );
