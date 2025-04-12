2c8747f05e05827267420687073669d7208a28ab
JonashGana <jonashtiglao@gmail.com>
2025-04-12 23:32:32 +0800

    added formatTime

--- lib/utils.ts
+++ lib/utils.ts
@@ -12,6 +12,14 @@ export const slugify = (str: string) => {
     .replace(/[^\w-]+/g, "");
 };
 
+export const formatTime = (time: number) => {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  return `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}`;
+};
+
 export const themes = [
   { name: "Snowy Winter Cabin", image: "/themes/snowy-winter-cabin.jpg" },
   { name: "Countryside Afternoon", image: "/themes/countryside-afternoon.jpg" },
