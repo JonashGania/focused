ba8306ac782a535346708568d84bf90c9c4621c6
JonashGana <jonashtiglao@gmail.com>
2025-04-12 23:33:43 +0800

    initial commit

---
+++ components/home/Hero.tsx
@@ -0,0 +1,11 @@
+import Pomodoro from "./Pomodoro";
+
+const Hero = () => {
+  return (
+    <div className="w-full px-8">
+      <Pomodoro />
+    </div>
+  );
+};
+
+export default Hero;
---
+++ components/home/Pomodoro.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { SemiCircularProgress } from "../progress-bars/SemiCircularProgress";
+import { Button } from "../ui/button";
+import { useTimer } from "@/hooks/use-timer";
+import { formatTime } from "@/lib/utils";
+
+const Pomodoro = () => {
+  const {
+    mode,
+    timeLeft,
+    totalTime,
+    isActive,
+    sessions,
+    toggleTimer,
+    resetTimer,
+    switchMode,
+  } = useTimer();
+
+  const progress = ((totalTime - timeLeft) / totalTime) * 100;
+
+  return (
+    <div className="flex flex-col justify-center items-center max-w-[400px] w-full mx-auto">
+      <div className="flex justify-between items-center w-full mb-8">
+        <span className="text-white text-2xl font-medium">
+          {mode === "focus" ? "🎯 Focus" : "☕ Break"} Time
+        </span>
+        <span className="text-xl text-gray-200 font-medium">
+          Sessions: {sessions}
+        </span>
+      </div>
+      <SemiCircularProgress
+        progress={progress}
+        size={400}
+        strokeWidth={20}
+        circleColor="oklch(81% 0.117 11.638)"
+        indicatorColor="oklch(64.5% 0.246 16.439)"
+        indicatorSize={15}
+        label={formatTime(timeLeft)}
+        showPercentage={false}
+      />
+      <div className="flex items-center gap-4 mt-8">
+        <Button
+          onClick={toggleTimer}
+          className={`${
+            isActive
+              ? "bg-yellow-500 hover:bg-yellow-500"
+              : "bg-rose-500 hover:bg-rose-500"
+          } h-11 w-28 text-lg cursor-pointer text-white rounded-md transform active:scale-95 transition-transform duration-150`}
+        >
+          {isActive ? "Pause" : "Start"}
+        </Button>
+        <Button
+          onClick={resetTimer}
+          className="bg-gray-200 hover:bg-gray-200 h-11 w-28 text-lg cursor-pointer text-black rounded-md transform active:scale-95 transition-transform duration-150"
+        >
+          Reset
+        </Button>
+      </div>
+      <div className="mt-4">
+        <Button
+          onClick={switchMode}
+          className="w-[400px] bg-neutral-900/70 hover:bg-neutral-900 h-10 text-white cursor-pointer"
+        >
+          Switch to {mode === "focus" ? "Break" : "Focus"}
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Pomodoro;
