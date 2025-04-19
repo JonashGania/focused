3d09482ff7cb3eb387238039def67bef3d68401b
JonashGana <jonashtiglao@gmail.com>
2025-04-19 23:57:47 +0800

    initial commit

---
+++ actions/tasks.ts
@@ -0,0 +1,77 @@
+"use server";
+
+import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
+
+export const addTask = async (formData: FormData) => {
+  const supabase = await createClient();
+  const taskInput = formData.get("task") as string | null;
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    throw new Error("User is not logged in");
+  }
+
+  const { error } = await supabase
+    .from("todos")
+    .insert({ user_id: user.id, task: taskInput });
+
+  if (error) {
+    throw new Error("Error adding task");
+  }
+
+  revalidatePath("/");
+};
+
+export const deleteTask = async (taskId: number) => {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    throw new Error("User is not logged in");
+  }
+
+  const { error } = await supabase
+    .from("todos")
+    .delete()
+    .match({ user_id: user.id, id: taskId });
+
+  if (error) {
+    throw new Error("Error deleting task");
+  }
+
+  revalidatePath("/");
+};
+
+export const updateTaskCompletion = async (
+  taskId: number,
+  isComplete: boolean
+) => {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    throw new Error("User is not logged in");
+  }
+
+  const { error } = await supabase
+    .from("todos")
+    .update({ is_complete: isComplete })
+    .eq("id", taskId)
+    .eq("user_id", user.id);
+
+  if (error) {
+    throw new Error("Error updating task completion");
+  }
+
+  revalidatePath("/");
+};
