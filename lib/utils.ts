bf40efb837aa695bfeb71a70bc4f6aafd3f8d001
JonashGana <jonashtiglao@gmail.com>
2025-04-19 23:57:12 +0800

    added taskReducer utility

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { Action, Tasks } from "@/types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -29,3 +30,21 @@ export const themes = [
   { name: "Sunset Valley", image: "/themes/sunset-valley.jpg" },
   { name: "Wisdom Forest", image: "/themes/wisdom-forest.jpg" },
 ];
+
+export const taskReducer = (
+  state: Tasks[] | null,
+  { action, task }: { action: Action; task: Tasks }
+) => {
+  if (!state) return [];
+
+  switch (action) {
+    case "delete":
+      return state.filter(({ id }) => id !== task.id);
+    case "updateTask":
+      return state.map(({ id }) =>
+        id === task.id ? { ...task, is_complete: task.is_complete } : task
+      );
+    default:
+      return state;
+  }
+};
