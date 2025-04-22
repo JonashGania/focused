0b84a76962e3a5fcea2d943bbb775ffe831dc030
JonashGana <jonashtiglao@gmail.com>
2025-04-22 22:46:09 +0800

    done

--- components/main/Header.tsx
+++ components/main/Header.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { User } from "@supabase/supabase-js";
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({ user }: { user: User }) => {
+  return (
+    <header className="py-8 px-12 flex justify-end">
+      <div className="">
+        <p className="text-gray-200 text-end text-2xl font-medium select-none">
+          &quot;Your focus today defines your <br /> success tomorrow,{" "}
+          <span className="font-bold text-white">
+            {user.user_metadata.first_name}
+          </span>
+          &quot;
+        </p>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
