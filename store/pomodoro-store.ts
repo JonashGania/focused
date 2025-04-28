192eef475269b5af17ad3da530d5f0ba8b7fadcd
JonashGana <jonashtiglao@gmail.com>
2025-04-28 22:35:04 +0800

    added more states

--- store/pomodoro-store.ts
+++ store/pomodoro-store.ts
@@ -4,8 +4,12 @@ import { createJSONStorage, persist } from "zustand/middleware";
 type PomodoroState = {
   focusTimer: number;
   breakTimer: number;
+  alertSound: string;
+  alertVolume: number;
   setFocusTimer: (time: number) => void;
   setBreakTimer: (time: number) => void;
+  setAlertSound: (sound: string) => void;
+  setAlertVolume: (volume: number) => void;
 };
 
 export const usePomodoroStore = create<PomodoroState>()(
@@ -13,8 +17,12 @@ export const usePomodoroStore = create<PomodoroState>()(
     (set) => ({
       focusTimer: 25,
       breakTimer: 5,
+      alertSound: "shine",
+      alertVolume: 0.5,
       setFocusTimer: (time: number) => set({ focusTimer: time }),
       setBreakTimer: (time: number) => set({ breakTimer: time }),
+      setAlertSound: (sound: string) => set({ alertSound: sound }),
+      setAlertVolume: (volume: number) => set({ alertVolume: volume }),
     }),
     {
       name: "pomodoro-settings",
