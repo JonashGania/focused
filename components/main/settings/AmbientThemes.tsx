03ac98aa9e8de98d0e04afc0fe9b684bad1bbfc1
JonashGana <jonashtiglao@gmail.com>
2025-04-28 22:35:50 +0800

    Ambient themes setting component

---
+++ components/main/settings/AmbientThemes.tsx
@@ -0,0 +1,11 @@
+import { themes } from "@/lib/utils";
+
+const AmbientThemes = () => {
+  return (
+    <div className="mt-8">
+      <h1 className="text-3xl text-white font-bold">Ambient Themes</h1>
+    </div>
+  );
+};
+
+export default AmbientThemes;
