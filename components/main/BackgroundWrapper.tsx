50c9a3eadbaed9a8af77fd6cc5f9ffe96a925a21
JonashGana <jonashtiglao@gmail.com>
2025-04-29 22:41:21 +0800

    used useThemeStore

--- components/main/BackgroundWrapper.tsx
+++ components/main/BackgroundWrapper.tsx
@@ -1,23 +1,18 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useThemeStore } from "@/store/theme-store";
 import Image from "next/image";
 
 const BackgroundWrapper = () => {
-  const [theme, setTheme] = useState<string | null>(null);
-
-  useEffect(() => {
-    const storedTheme = localStorage.getItem("theme");
-    if (storedTheme) setTheme(storedTheme);
-  }, []);
+  const backgroundTheme = useThemeStore((state) => state.backgroundTheme);
 
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 object-cover object-center -z-10 ">
       <div className="h-full w-full relative">
         <div className="absolute inset-0 bg-black opacity-30 z-10"></div>
         <Image
-          src={`/themes/${theme}.jpg`}
-          alt={`${theme} theme`}
+          src={`/themes/${backgroundTheme}.jpg`}
+          alt={`${backgroundTheme} theme`}
           fill
           className="object-cover object-center transition-opacity duration-1000 ease-in"
         />
