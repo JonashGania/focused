5912eb42685308fb2d49ccbbb90a70bca09d6f0a
JonashGana <jonashtiglao@gmail.com>
2025-04-29 22:39:28 +0800

    import Hydrated

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 import BackgroundWrapper from "@/components/main/BackgroundWrapper";
+import Hydrated from "@/components/main/Hydrated";
 import Hero from "@/components/main/Hero";
 import Header from "@/components/main/Header";
 import Footer from "@/components/main/Footer";
@@ -14,13 +15,13 @@ export default async function Home() {
   }
 
   return (
-    <>
+    <Hydrated>
       <BackgroundWrapper />
       <div className="w-full flex flex-col min-h-screen font-[family-name:var(--font-roboto)]">
         <Header user={data.user} />
         <Hero user={data.user} />
         <Footer />
       </div>
-    </>
+    </Hydrated>
   );
 }
