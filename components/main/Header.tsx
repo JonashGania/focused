11b6d837ca90725a7b27a4257c2c9f7be63a7fc4
JonashGana <jonashtiglao@gmail.com>
2025-04-29 22:42:06 +0800

    fixed padding

--- components/main/Header.tsx
+++ components/main/Header.tsx
@@ -2,9 +2,9 @@ import { User } from "@supabase/supabase-js";
 
 const Header = ({ user }: { user: User }) => {
   return (
-    <header className="py-8 px-12 flex justify-end">
+    <header className="py-8 px-4 min-[450px]:px-8 min-[500px]:px-12 flex justify-end">
       <div className="">
-        <p className="text-gray-200 text-end text-2xl font-medium select-none">
+        <p className="text-gray-200 text-end text-xl min-[450px]:text-2xl font-medium select-none">
           &quot;Your focus today defines your <br /> success tomorrow,{" "}
           <span className="font-bold text-white">
             {user.user_metadata.first_name}
