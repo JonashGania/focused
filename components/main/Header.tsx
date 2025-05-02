e9227b38cfe4aadd15dadf82bf517ffd310db6e9
JonashGana <jonashtiglao@gmail.com>
2025-05-02 23:09:52 +0800

    added animation to p tag

--- components/main/Header.tsx
+++ components/main/Header.tsx
@@ -1,16 +1,24 @@
+"use client";
+
 import { User } from "@supabase/supabase-js";
+import { motion } from "motion/react";
 
 const Header = ({ user }: { user: User }) => {
   return (
     <header className="py-8 px-4 min-[450px]:px-8 min-[500px]:px-12 flex justify-end">
       <div className="">
-        <p className="text-gray-200 text-end text-xl min-[450px]:text-2xl font-medium select-none">
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.3, ease: "easeInOut" }}
+          className="text-gray-200 text-end text-xl min-[450px]:text-2xl font-medium select-none"
+        >
           &quot;Your focus today defines your <br /> success tomorrow,{" "}
           <span className="font-bold text-white">
             {user.user_metadata.first_name}
           </span>
           &quot;
-        </p>
+        </motion.p>
       </div>
     </header>
   );
