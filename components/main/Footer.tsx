a329617ffd064ee8c15cb975858fb3f71c0a1782
JonashGana <jonashtiglao@gmail.com>
2025-05-15 22:02:18 +0800

    added popover

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { Music, Settings, LogOut } from "lucide-react";
+import { Music, Settings, LogOut, User } from "lucide-react";
 import { signout } from "@/actions/auth";
 import { useRef, useState } from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import SoundscapeWrapper from "./soundscape/SoundscapeWrapper";
 import SettingsSidebar from "./settings/SettingsSidebar";
 
@@ -27,11 +28,36 @@ const Footer = () => {
         >
           <Music size={20} strokeWidth={2} className="text-white" />
         </button>
-        <form action={signout}>
+        <Popover>
+          <PopoverTrigger asChild>
+            <button className="bg-neutral-900 p-3 rounded-md cursor-pointer border border-white/30 transform active:scale-90 transition-all duration-150 hover:bg-neutral-800">
+              <User size={20} strokeWidth={2} className="text-white" />
+            </button>
+          </PopoverTrigger>
+          <PopoverContent className="w-[180px] p-3 mb-2">
+            <div className="grid gap-2">
+              <h4 className="text-black font-medium">My Account</h4>
+              <form action={signout}>
+                <button className="flex items-center gap-2 group cursor-pointer">
+                  <LogOut
+                    size={18}
+                    strokeWidth={2}
+                    className="text-red-500 group-hover:text-black"
+                  />
+                  <span className="text-red-500 group-hover:text-black">
+                    Log out
+                  </span>
+                </button>
+              </form>
+            </div>
+          </PopoverContent>
+          {/* <form action={signout}>
           <button className="bg-neutral-900 p-3 rounded-md cursor-pointer border border-white/30 transform active:scale-90 transition-all duration-150 hover:bg-neutral-800">
             <LogOut size={20} strokeWidth={2} className="text-white" />
           </button>
-        </form>
+        </form> */}
+        </Popover>
+
         <SoundscapeWrapper
           isOpen={openSounds}
           setOpen={setOpenSounds}
