7f379a6f651b056eddcd4c929ac9ff46f0f49512
JonashGana <jonashtiglao@gmail.com>
2025-09-09 22:17:37 +0800

    ui improvements

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = async ({ user }: { user: User }) => {
     .order("inserted_at", { ascending: true });
 
   return (
-    <div className="flex-1 w-full px-4 min-[450px]:px-8 select-none">
+    <div className="flex-1 w-full px-4 min-[450px]:px-8 select-none pb-12">
       <Pomodoro />
       <PrioritiesDialog tasks={tasks} />
     </div>
