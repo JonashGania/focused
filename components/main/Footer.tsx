af1e715a6b71b71f36d81c04cc8bd44c0c382aa7
JonashGana <jonashtiglao@gmail.com>
2025-09-09 22:16:43 +0800

    signout ui change

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -34,28 +34,17 @@ const Footer = () => {
               <User size={20} strokeWidth={2} className="text-white" />
             </button>
           </PopoverTrigger>
-          <PopoverContent className="w-[180px] p-3 mb-2">
+          <PopoverContent className="w-[180px] p-0 py-3 mb-2">
             <div className="grid gap-2">
-              <h4 className="text-black font-medium">My Account</h4>
+              <h4 className="text-black font-medium px-3">My Account</h4>
               <form action={signout}>
-                <button className="flex items-center gap-2 group cursor-pointer">
-                  <LogOut
-                    size={18}
-                    strokeWidth={2}
-                    className="text-red-500 group-hover:text-black"
-                  />
-                  <span className="text-red-500 group-hover:text-black">
-                    Log out
-                  </span>
+                <button className="flex items-center gap-2 group cursor-pointer hover:bg-red-500/10 w-full py-1 px-3">
+                  <LogOut size={18} strokeWidth={2} className="text-red-500" />
+                  <span className="text-red-500">Log out</span>
                 </button>
               </form>
             </div>
           </PopoverContent>
-          {/* <form action={signout}>
-          <button className="bg-neutral-900 p-3 rounded-md cursor-pointer border border-white/30 transform active:scale-90 transition-all duration-150 hover:bg-neutral-800">
-            <LogOut size={20} strokeWidth={2} className="text-white" />
-          </button>
-        </form> */}
         </Popover>
 
         <SoundscapeWrapper
